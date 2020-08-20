582e9cdd02a09cba05ae53143bcc727198684e86
Paul Lee <paul.lee0627@gmail.com>
2020-08-19 18:47:41 -0700

    finished queue

--- queue/queue.py
+++ queue/queue.py
@@ -46,3 +46,9 @@ class Queue:
     def enqueue(self, value):
         self.storage.add_to_tail(value)
         self.size += 1
+
+    def dequeue(self):
+        if self.size == 0:
+            return None
+        self.size -= 1
+        return self.storage.remove_head()

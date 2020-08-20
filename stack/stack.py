37fa2af3218b458973f09632dce7fa1ef0903cff
Paul Lee <paul.lee0627@gmail.com>
2020-08-19 18:40:02 -0700

    finished problem 2 in stack

--- stack/stack.py
+++ stack/stack.py
@@ -12,7 +12,9 @@ return elements in Last In First Out order.
     
     The methods to add and remove values are different.
 """
-class Stack:
+
+# This is for the first problem
+""" class Stack:
     def __init__(self):
         self.size = 0
         # self.storage = ?
@@ -28,4 +30,23 @@ class Stack:
     def pop(self):
         if len(self.storage) == 0:
             return None
-        return self.storage.pop()
+        return self.storage.pop() """
+
+from singly_linked_list import LinkedList
+class Stack:
+    def __init__(self):
+        self.size = 0
+        self.storage = LinkedList()
+
+    def __len__(self):
+       return self.size
+
+    def push(self, value):
+        self.storage.add_to_tail(value)
+        self.size += 1
+
+    def pop(self):
+        if self.size == 0:
+            return None
+        self.size -= 1
+        return self.storage.remove_tail()

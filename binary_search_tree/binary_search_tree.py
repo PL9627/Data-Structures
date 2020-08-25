221ffd82872ae38a348f1e9f1f8c9634ae208de8
Paul Lee <paul.lee0627@gmail.com>
2020-08-25 16:27:39 -0700

    get max passes

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -48,7 +48,12 @@ class BSTNode:
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        current = self
+
+        while current.right is not None:
+            current = current.right
+
+        return current.value
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
